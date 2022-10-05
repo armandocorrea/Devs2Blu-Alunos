2c9de5f8fc2b51522a367d53cc5f91a52e654276
FURB\michaelrodrigo <michaelsilva246@gmail.com>
2022-10-05 18:53:15 -0300

    Complementando ex 24

--- Michael/ufRmCategoria.pas
+++ Michael/ufRmCategoria.pas
@@ -36,27 +36,37 @@ end;
 
 procedure TForm1.CategoriaNadador;
 var
-  xIdade : Integer;
-  xCategoria : String;
+  xIdade : array of Integer;
+  xCategoria : array of String;
+  xQtdAtletas, I : Integer;
 
 begin
 
-  xIdade:= StrToInt(inputbox('Produto', 'Digite a Idade', ''));
+  xQtdAtletas := StrToInt(inputbox('Entradas', 'Quantidade a Classificar ', ''));
+  SetLength(xIdade,xQtdAtletas);
+  SetLength(xCategoria,xQtdAtletas);
 
-  if (xIdade >= 5) and (xIdade <= 7) then
-    xCategoria := ' Categoria Infantil A. '
-  else if (xIdade >= 8) and (xIdade <= 10) then
-     xCategoria := ' Categoria Infantil B. '
-  else if (xIdade >= 11) and (xIdade <= 13) then
-     xCategoria := ' Categoria Juvenil A. '
-  else if (xIdade >= 14) and (xIdade <= 17) then
-     xCategoria := ' Categoria Juvenil B. '
-  else if (xIdade >= 18) and (xIdade <= 25) then
-     xCategoria := ' Sênior. '
-  else
-    xCategoria := ' Idade fora da faixa etária. ';
+  for I := 0 to xQtdAtletas-1 do
+  begin
 
-  mmResultado.lines.add(xCategoria);
+    xIdade[I]:= StrToInt(inputbox('Idade', 'Digite a Idade ', ''));
+
+    if (xIdade[I] >= 5) and (xIdade[I] <= 7) then
+      xCategoria[I]  := ' Categoria Infantil A. '
+    else if (xIdade[I] >= 8) and (xIdade[I] <= 10) then
+      xCategoria[I]  := ( ' Categoria Infantil B. ')
+    else if (xIdade[I] >= 11) and (xIdade[I] <= 13) then
+      xCategoria[I]  := ' Categoria Juvenil A. '
+    else if (xIdade[I] >= 14) and (xIdade[I] <= 17) then
+      xCategoria[I]  := ' Categoria Juvenil B. '
+    else if (xIdade[I] >= 18) and (xIdade[I] <= 25) then
+      xCategoria[I]  := ' Categoria Sênior. '
+    else
+      xCategoria[I]  := ' Idade fora da faixa etária. ';
+
+  end;
+  for I := 0 to Length(xCategoria)-1 do
+  mmResultado.lines.add(xCategoria[I]);
 
 
 end;
